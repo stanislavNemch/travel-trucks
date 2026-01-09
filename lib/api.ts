981327d611ea416eae3c64dd27601bd59ac8b35f
Stanislav Nemchenko <stas.buka@gmail.com>
2026-01-09 03:12:04 +0200

    feat: update API base URL to use environment variable

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const instance = axios.create({
-    baseURL: "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io",
+  baseURL: BASE_URL,
 });
 
 export const getCampers = async (params?: Record<string, any>) => {
-    const { data } = await instance.get("/campers", { params });
-    return data;
+  const { data } = await instance.get("/campers", { params });
+  return data;
 };
 
 export const getCamperById = async (id: string) => {
-    const { data } = await instance.get(`/campers/${id}`);
-    return data;
+  const { data } = await instance.get(`/campers/${id}`);
+  return data;
 };
